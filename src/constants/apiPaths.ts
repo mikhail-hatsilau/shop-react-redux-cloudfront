2f988861521184b6e738991e0249f281f8b0ad65
Mikhail Hatsilau <Mikhail_Hatsilau@epam.com>
2022-10-21 19:29:51 +0300

    updated gateway url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://27wh4vosp4.execute-api.eu-west-1.amazonaws.com/dev/api",
+  product: "https://jwob72q496.execute-api.eu-west-1.amazonaws.com/dev/api",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://kploxqe0p8.execute-api.eu-west-1.amazonaws.com/dev/api",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
